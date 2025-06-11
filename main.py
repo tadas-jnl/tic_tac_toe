d79e121a31c07aafc751137ebe3941a347993701
tadas-jnl <tjnl@proton.me>
2025-06-11 13:28:28 +0300

    Added main menu. Added Colors

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from models import Player, Board
+import pyinputplus as pyip
+from rich.console import Console
+
 lucky = [[1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+console = Console()
 
 def luck_check(player, lucky=lucky):
     for lucky_set in lucky:
@@ -8,44 +12,67 @@ def luck_check(player, lucky=lucky):
             if arg in player.numbers:
                 checker += 1
                 if checker == 3:
-                    player.status = True
-                    print(f"{player.name} wins!")
+                    player.iswinner = True
+                    console.print(f"[bold blue]{player.name}[/bold blue] wins!", style="bold green")
+                    return False
                 pass
 
 def get_num(player):
     while True:
         try:
-            num = int(input(f"{player.name} turn: "))
+            console.print(f"[bold blue]{player.name}[/bold blue] turn: ", style="bold green")
+            num = int(input())
             if num < 0 or num > 9:
-                print("Number is not on the board!")
+                console.print("Number is not on the board!", style="bold red")
                 continue
             if num in all_numbers:
-                print("This field is taken!")
+                console.print("This field is taken!", style="bold red")
                 continue
             all_numbers.append(num)
             return num
         except ValueError:
-            print("Your choice is not a number!")
+            console.print("Your choice is not a number!", style="bold red")
+
+def play():
+    game_board = Board
+    game_board.positions = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    game_board.draw(game_board, all_numbers)
+    all_numbers.clear()
+    player_x.numbers = []
+    player_o.numbers = []
+    player_x.iswinner = False
+    player_o.iswinner = False
+    console.print("\n\n\n\n\nThis is 'Tic Tac Toe' game. You know the rules.\n", style="bold blue")
+    while not player_x.iswinner or player_o.iswinner:
+        if len(all_numbers) == 9: break
+        for plr in players:
+            if len(all_numbers) == 9:
+                console.print("Draw!", style="bold cyan")
+                break
+            plr.numbers.append(get_num(plr))
+            print("\n" * 10)
+            game_board.board_fill(game_board, all_numbers, player_x.numbers, player_o.numbers)
+            game_board.draw(game_board)
+            luck_check(plr)
+            if plr.iswinner: break
 
 player_x = Player("Player X")
 player_o = Player("Player O")
 players = [player_x, player_o]
 all_numbers = []
-game_board = Board
 
-print("This is 'Tic Tac Toe' game. You know the rules.")
-game_board.draw(game_board, all_numbers)
 
-while not player_x.status or player_o.status:
-    if len(all_numbers) == 9: break
-    for plr in players:
-        if len(all_numbers) == 9:
-            print("Draw!")
-            break
-        plr.numbers.append(get_num(plr))
-        print("\n" * 10)
-        game_board.board_fill(game_board, all_numbers, player_x.numbers, player_o.numbers)
-        game_board.draw(game_board)
-        luck_check(plr)
-        if plr.status: break
-
+if __name__ == "__main__":
+    while True:
+        print("\n")
+        console.print("1 - Start game", style="bold blue")
+        console.print("2 - Exit", style="bold blue")
+        menu = input()
+        match menu:
+            case "1":
+                play()
+            case "2":
+                console.print("Exiting the game. Goodbye!", style="bold red")
+                break
+            case _:
+                console.print("Invalid option. Please try again.", style="bold red")
--- models.py
+++ models.py
@@ -1,19 +1,21 @@
 from dataclasses import dataclass, field
+from rich.console import Console
+console = Console()
 @dataclass()
 class Player:
     name: str
     numbers: list = field(default_factory=list)
-    status: bool = False
+    iswinner: bool = False
 
 @dataclass()
 class Board:
     positions = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     def draw(self, positions = positions):
-        print(f" {self.positions[0]} | {self.positions[1]} | {self.positions[2]}")
-        print("---+---+---")
-        print(f" {self.positions[3]} | {self.positions[4]} | {self.positions[5]}")
-        print("---+---+---")
-        print(f" {self.positions[6]} | {self.positions[7]} | {self.positions[8]}")
+        console.print(f" {self.positions[0]} [bold green]|[/bold green] {self.positions[1]} [bold green]|[/bold green] {self.positions[2]}")
+        console.print("---+---+---", style="bold green")
+        console.print(f" {self.positions[3]} [bold green]|[/bold green] {self.positions[4]} [bold green]|[/bold green] {self.positions[5]}")
+        console.print("---+---+---", style="bold green")
+        console.print(f" {self.positions[6]} [bold green]|[/bold green] {self.positions[7]} [bold green]|[/bold green] {self.positions[8]}\n")
     def board_fill(self, xo, playerX, playerO):
         for x in xo:
             if x in playerX:
